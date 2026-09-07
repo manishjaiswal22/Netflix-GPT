8425227d690aa072b6ea7c758e40d9b6789afe12
Manish Jaiswal <jmanish222@gmail.com>
2026-09-07 12:57:18 +0530

    Netflix GPT

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1 className='text-3xl text-red-500 font-bold'>Hello</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
